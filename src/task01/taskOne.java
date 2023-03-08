7ff4b80183e4f8fc159415114bde4c8074b5d795
NemuiLiz <nemui.liz@gmx.de>
2023-03-08 13:28:04 +0100

    Test

---
+++ src/task01/taskOne.java
@@ -0,0 +1,17 @@
+package task01;
+
+public class taskOne {
+    int a = 5;
+    double b = 5.0;
+    long c = 4L;
+    int d = (3+9); // 12
+    double e = (2 + 6.0); //8.0
+    float f = (5 + 3.0f); //8.0
+    //g??
+    double g = (3d + 1.0f); //4.0
+    long h = (6L *3); //18L
+    int i = (7 /2); //3
+    double j = (9/2.0); //4.5
+    // k?
+    int k = 1;
+}
